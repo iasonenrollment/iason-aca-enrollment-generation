b7a4091a829e4fd7528cc81500b9c6acdd403e03
Balaji Varadharajan <vbalaji215@gmail.com>
2020-05-04 20:20:29 -0400

    initial commit

---
+++ src/main/java/com/iason/aca/enrollment/generation/IasonAcaEnrollmentGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.iason.aca.enrollment.generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasonAcaEnrollmentGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IasonAcaEnrollmentGenerationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iason/aca/enrollment/generation/IasonAcaEnrollmentGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iason.aca.enrollment.generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasonAcaEnrollmentGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
